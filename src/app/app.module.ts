f348769d682876a0f9b74c1818b78503a96e8aef
Diakahril <dhia.kahri@lobsterware.com>
2022-01-22 20:27:34 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { NoteInputComponent } from './note-input/note-input.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NotePreviewComponent } from './note-preview/note-preview.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    SideNavComponent,
+    NoteInputComponent,
+    NoteListComponent,
+    NotePreviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
