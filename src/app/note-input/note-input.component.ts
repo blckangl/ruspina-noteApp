57ae739fb2822fb0672aa9f574e6bc21e71982ff
Diakahril <dhia.kahri@lobsterware.com>
2022-01-22 21:24:25 +0100

    localreference,viewchild

--- src/app/note-input/note-input.component.ts
+++ src/app/note-input/note-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Note} from "../shared/interfaces/note";
 
 @Component({
@@ -6,21 +6,40 @@ import {Note} from "../shared/interfaces/note";
   templateUrl: './note-input.component.html',
   styleUrls: ['./note-input.component.scss']
 })
-export class NoteInputComponent implements OnInit {
+export class NoteInputComponent implements OnInit,AfterViewInit {
 
   @Output() onNoteAdded: EventEmitter<Note> = new EventEmitter<any>()
-
+  @ViewChild('noteContentInput') noteContentInput!:ElementRef;
+  titleInput: string = '';
 
 
   constructor() {
+    console.log(this.noteContentInput);
+    console.log("constructor called");
   }
 
   ngOnInit(): void {
+    console.log("ngOnInit called");
+    console.log(this.noteContentInput);
+
   }
 
-  addNote(title:string,content:string) {
+  addNote() {
 
-    this.onNoteAdded.emit({id: 0, title: title, content:content, isDone: false})
+    this.onNoteAdded.emit({id: 0, title: this.titleInput, content: this.noteContentInput.nativeElement.value, isDone: false})
+    this.titleInput = '';
+    this.noteContentInput.nativeElement.value = '';
+
   }
 
+  checkIfHasNoValue(str: string) {
+    if (str.length == 0)
+      return true;
+    return false
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called");
+    console.log(this.noteContentInput);
+  }
 }
